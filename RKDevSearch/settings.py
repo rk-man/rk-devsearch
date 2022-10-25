42b209c58ffd5723aac1fd65d55bd85f8bc1aa86
rk_man <naveenbadi2003@gmail.com>
2022-10-25 12:24:51 +0530

    saving files in staticfiles

--- RKDevSearch/settings.py
+++ RKDevSearch/settings.py
@@ -195,7 +195,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # to tell Django where to find media files
 MEDIA_URL = "/images/"
