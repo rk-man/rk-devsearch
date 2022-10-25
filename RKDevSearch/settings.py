25db098b21d25c5c085765472ac9fa483d979eec
rk_man <naveenbadi2003@gmail.com>
2022-10-25 12:09:06 +0530

    setting allowed hosts to all

--- RKDevSearch/settings.py
+++ RKDevSearch/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = 'django-insecure-laqkv_l4@c4xuak_4*cbun^fmpl=)b$r_=4htp*(x&gsb4u2&^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1',
-                 'https://rk-devsearch.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 # allow all origins
 CORS_ALLOW_ALL_ORIGINS = True
